e6c4b6a62b783a6f9dc9413af0e53b9b1ff291ee
diorm47 <dior47m@gmail.com>
2024-06-08 18:58:42 +0500

    file storage, code updates

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
-import * as UserController from "./controllers/user-controller.js";
+import multer from "multer";
 import * as GridsController from "./controllers/grids-controller.js";
+import * as UserController from "./controllers/user-controller.js";
 import checkAuth from "./utils/check-auth.js";
+import handleValidationError from "./utils/handleValidationError.js";
 import {
-  registerValidation,
-  loginValidation,
   gridCreateValidation,
+  loginValidation,
+  registerValidation,
 } from "./validations/validations.js";
+
 const PORT = 3001;
 
 mongoose
@@ -21,21 +24,61 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
 app.get("/", (req, res) => {
   res.json("Server is working");
 });
 // Login/registration
-app.post("/login", loginValidation, UserController.login);
-app.post("/register", registerValidation, UserController.register);
+app.post(
+  "/login",
+  loginValidation,
+  handleValidationError,
+  UserController.login
+);
+app.post(
+  "/register",
+  registerValidation,
+  handleValidationError,
+  UserController.register
+);
 app.get("/me", checkAuth, UserController.me);
 
 // Grids CRUD
 app.get("/grids", GridsController.getAll);
 app.get("/grids/:id", GridsController.getOne);
-app.post("/grids", checkAuth, gridCreateValidation, GridsController.create);
+app.post(
+  "/grids",
+  checkAuth,
+  gridCreateValidation,
+  handleValidationError,
+  GridsController.create
+);
 app.delete("/grids/:id", checkAuth, GridsController.remove);
-app.patch("/grids/:id", checkAuth, GridsController.update);
+app.patch(
+  "/grids/:id",
+  checkAuth,
+  gridCreateValidation,
+  handleValidationError,
+  GridsController.update
+);
 
 app.listen(PORT, (err) => {
   if (err) {
